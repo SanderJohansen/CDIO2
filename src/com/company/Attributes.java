6cbeb919b2f17f8a2a432a4ac6cd44de106e425e
Sander Johansen <sander49@live.dk>
2019-10-07 14:38:44 +0200

    Prøve/anden commit

---
+++ src/com/company/Attributes.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Attributes {
+
+    String name;
+    int points;
+    String extra;
+
+    public Field(String name, int points, String extra){
+        System.out.println("You landed on " + name + " Which is " + points + " points" );
+
+    }
+
+
+
+    public rules
+}
+
+
+
+
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is player 1´s name?");
+        String player1 = scan.nextLine();
+        System.out.println("\"What is player 2´s name?\"");
+        String player2 = scan.nextLine();
+
+
+        System.out.println("Welcome " + player1 + " and " + player2);
+
+
+
+
+
+
     }
 }
