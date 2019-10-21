19a6cf26646e1ba81d471ea327b88a989e123f7a
JonasHenriksen123 <56156281+JonasHenriksen123@users.noreply.github.com>
2019-10-21 15:07:19 +0200

    Diceroller lavet

--- src/com/company/Diceroller.java
+++ src/com/company/Diceroller.java
@@ -1,7 +1,33 @@
 package com.company;
 
 public class Diceroller {
-    int die1;
-    int die2;
+    private int die1;
+    private int die2;
 
+    // konstruerer klassen og ruller terningerne
+    public Diceroller() {
+        roll();
+    }
+
+    // ruller terningerne til tilfædig værdi mellem 1 og 6
+    public void roll(){
+        die1 = (int)(Math.random() * 6) + 1;
+        die2 = (int)(Math.random() * 6) + 1;
+    }
+
+    // henter værdien af terning 1
+    public int face_die1(){
+        return die1;
+    }
+
+    // henter værdien af terning 2
+    public int face_die2(){
+        return die2;
+    }
+
+    // henter summen af terningerne
+    public int face_sum(){
+        int sum = die1 +die2;
+        return sum;
+    }
 }
