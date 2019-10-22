e3b2a37c5836577b9885d77be72f5a9f7cb6534a
JonasHenriksen123 <56156281+JonasHenriksen123@users.noreply.github.com>
2019-10-22 15:13:50 +0200

    metoder ændret fra static

--- src/com/company/Diceroller.java
+++ src/com/company/Diceroller.java
@@ -2,8 +2,8 @@ package com.company;
 
 
 public class Diceroller {
-    private static int die1;
-    private static int die2;
+    private int die1;
+    private int die2;
 
     // konstruerer klassen og ruller terningerne
     public Diceroller() {
@@ -11,24 +11,23 @@ public class Diceroller {
     }
 
     // ruller terningerne til tilfædig værdi mellem 1 og 6
-    public static void roll(){
+    public void roll(){
         die1 = (int)(Math.random() * 6) + 1;
         die2 = (int)(Math.random() * 6) + 1;
     }
 
     // henter værdien af terning 1
-    public static int face_die1(){
+    public int face_die1(){
         return die1;
     }
 
     // henter værdien af terning 2
-    public static int face_die2(){
+    public int face_die2(){
         return die2;
     }
 
     // henter summen af terningerne
-    public static int face_sum(){
-        int sum = die1 +die2;
-        return sum;
+    public int face_sum(){
+        return die1 + die2;
     }
 }
