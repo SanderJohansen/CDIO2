8952812f0878b55477665153cc0395147fc2ed67
JonasHenriksen123 <56156281+JonasHenriksen123@users.noreply.github.com>
2019-11-01 12:34:50 +0100

    Revert "test af klasse die ændret så den ruller og tester værdien 1000 gange."
    This reverts commit 4201e5ae6c8dc1928bb7cc48243709226a24fffe.

--- src/Tests/DieTest.java
+++ src/Tests/DieTest.java
@@ -12,10 +12,8 @@ public class DieTest {
 
     @org.junit.Test
     public void getFacevalue() {
-        for(int a = 0; a <1000; a++) {
-            DieTest.Roll();
-            int i = DieTest.GetFacevalue();
-            junit.framework.Assert.assertTrue(i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6);
-        }
+        int i = DieTest.GetFacevalue();
+
+        junit.framework.Assert.assertTrue(i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i ==6);
     }
 }
